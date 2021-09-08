9acdcb14e49ea938ebde35b69b27168cb288feea
GarrettF <grfoster24@gmail.com>
2021-09-08 15:16:32 -0400

    Added a button to see post details

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { getPosts } from "../../services/post"
 import Layout from "../../components/Layout/Layout"
+import { Link } from "react-router-dom"
 
 export default function Posts() {
   const [posts, setPosts] = useState([])
@@ -17,7 +18,12 @@ export default function Posts() {
     <Layout>
       <div>
         {posts.map((post, index) => (
-          <p>{post.title}</p>
+          <div className="post" key={index}>
+            <p>{post.title}</p>
+            <Link to={`/posts/${post._id}`}>
+              <button>See Details</button>
+            </Link>
+          </div>
         ))}
       </div>
     </Layout>
